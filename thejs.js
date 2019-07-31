9a4462513cc7bde737f61aa9bbdcf442cc9c887f
tiffamym <52763284+tiffamym@users.noreply.github.com>
2019-07-31 00:45:20 -0700

    Update thejs.js

--- thejs.js
+++ thejs.js
@@ -3,9 +3,10 @@ var zomato_key = "41ed66d99a210899ae3e1da817f06d16";
 
 var bobaCode = 247;
 var dessertCode = 100;
-var casualCode = 16;
+var fastCasualCode = 285;
+var fineDiningCode = 18;
 var vegetarianCode = 308;
-var healthyFoodCode = 143; 
+var healthyFoodCode = 143;
 
 var cuisineCodes = [];
 var estCodes = [];
@@ -16,10 +17,11 @@ var myRatings = [];
 // Detecting checkbox checks:
 //https://stackoverflow.com/questions/6878757/how-to-listen-to-when-a-checkbox-is-checked-in-jquery/6878786
 var checkboxes , checkboxArray;
-var allCodes = ["boba", "desserts", "vegetarian", "healthy food"];
+var allCodes = {"bubble tea": bobaCode, "desserts": dessertCode,
+                "vegetarian": vegetarianCode, "healthy food": healthyFoodCode};
 var allLocations = ["San Francisco", "Daly City", "Burlingame", "Millbrae", "San Bruno", "Redwood City", "Palo Alto", "Sunnyvale", "San Jose"];
-var allEstablishments = ["casual dining"];
-var allRatings = ["5", "4"];
+var allEstablishments = {"fast casual dining": fastCasualCode, "fine dining": fineDiningCode};
+var allRatings = ["5", "4", "3", "2", "1"];
 
 function confirmCheck() {
   if (this.checked) {
@@ -37,24 +39,57 @@ function confirmCheck() {
       console.log(locations);
       getResults();
     }
-    else if (allCodes.includes(val)) {
-      cuisineCodes.push(val);
+    // see if the string is in our allCodes dictionary
+    else if (Object.keys(allCodes).includes(val)) {
+      cuisineCodes.push(allCodes[val]);
       console.log(cuisineCodes);
       getResults();
     }
-    else if (allEstablishments.includes(val)) {
-      estCodes.push(val);
+    else if (Object.keys(allEstablishments).includes(val)) {
+      estCodes.push(allEstablishments[val]);
       console.log(estCodes);
       getResults();
     }
     else if (allRatings.includes(val)) {
       myRatings.push(val);
       if (allLocations.length > 0) {
-        sortByRating();
+      sortByRating();
       }
     }
   }
-}
+
+  else {
+    // checkbox has been unchecked
+    // alert("unchecked");
+
+    var val = this.value;
+    val = val.replace(/\_/g, " ");
+
+    // filter out the value that was unchecked
+    if (locations.length > 0) {
+      locations = locations.filter(
+        function(value, index, arr){
+          return value != val;
+      });
+      getResults();
+    }
+
+    if (cuisineCodes.length > 0) {
+      cuisineCodes = cuisineCodes.filter(
+        function(value, index, arr) {
+          return value != val;
+        });
+    }
+
+    if (myRatings.length > 0) {
+      myRatings = myRatings.filter(
+        function(value, index, arr) {
+          return value != val;
+        });
+      sortByRating();
+    }
+   }
+  }
 
 function getCheckboxes() {
   checkboxes = document.querySelectorAll('input[type=checkbox]');
@@ -67,10 +102,20 @@ function getCheckboxes() {
   console.log(checkboxes);
 }
 
+function uncheckCheckboxes() {
+  unchecked = document.querySelectorAll('input[type=checkbox]');
+  uncheckedArray = Array.from( unchecked );
+
+  uncheckedArray.forEach(function(unchecked) {
+    unchecked.addEventListener('change', confirmCheck);
+  });
+
+  console.log(unchecked);
+}
 
 
-function getBoba(bob) {
-  
+function getBubbleTea(bob) {
+  resultsDiv.innerHTML =""
   //var resultsDiv = document.getElementById("results");
 
   bob = bob.replace(/ /g, "%20");
@@ -80,12 +125,10 @@ function getBoba(bob) {
 
   console.log(reqURL);
 
-  var boba_id = 247;
 }
 
 function getDesserts(des) {
-  
-  //var resultsDiv = document.getElementById("results");
+  resultsDiv.innerHTML = ""
 
   des = des.replace(/ /g, "%20");
 
@@ -93,27 +136,21 @@ function getDesserts(des) {
   reqURL = reqURL + "q=" + des;
 
   console.log(reqURL);
-
-  var dessert_id = 100;
 }
 
-function getCasualDining(cas) {
-  
-  //var resultsDiv = document.getElementById("results");
+function getFastCasualDining(cas) {
+  resultsDiv.innerHTML = ""
 
   cas = cas.replace(/ /g, "%20");
 
-  var reqURL = "https://developers.zomato.com/api/v2.1/cuisines?";
+  var reqURL = "https://developers.zomato.com/api/v2.1/establishments?";
   reqURL = reqURL + "q=" + cas;
 
   console.log(reqURL);
-
-  var dining_id = 16;
 }
 
 function getVegetarian(veg) {
-  
-  //var resultsDiv = document.getElementById("results");
+  resultsDiv.innerHTML = ""
 
   veg = veg.replace(/ /g, "%20");
 
@@ -121,38 +158,34 @@ function getVegetarian(veg) {
   reqURL = reqURL + "q=" + veg;
 
   console.log(reqURL);
-
-  var vegetarian_id = 308;
 }
 
+function getHealthyFood(hea) {
+  resultsDiv.innerHTML = ""
+
+  hea = hea.replace(/ /g, "%20");
+
+  var reqURL = "https://developers.zomato.com/api/v2.1/establishments?";
+  reqURL = reqURL + "q=" + hea;
+
+  console.log(reqURL);
+}
 
 function getResults() {
+  myResults = [];
   locations.forEach(function(loc) {
     var city_id = getLocations(loc);
     // console.log(city_id);
   });
-  cuisineCodes.forEach(function(bob) {
-    var boba_id = getBoba(bob);
-    //console.log(bobaCode);
- });
-  cuisineCodes.forEach(function(des) {
-    var dessert_id = getDesserts(des);
-    console.log(dessertCode);
-}); 
-  cuisineCodes.forEach(function(cas) {
-    var dining_id = getCasualDining(cas);
-    console.log(casualCode);
-});
-cuisineCodes.forEach(function(veg) {
-    var vegetarian_id = getVegetarian(veg);
-    console.log(vegetarianCode);
-});  
+
+  displayResults();
 }
 
-
 function getLocations(loc) {
-  
+
+
   var resultsDiv = document.getElementById("results");
+  resultsDiv.innerHTML =""
 
   loc = loc.replace(/ /g, "%20");
 
@@ -172,9 +205,11 @@ function getLocations(loc) {
             // console.log(data);
             // get the city ID from the result
             city_id = data["location_suggestions"][0]["id"];
+            // boba_id = data["location_suggestions"][0]["id"];
 
             var reqURL = "https://developers.zomato.com/api/v2.1/search?";
             reqURL = reqURL + "entity_id=" + city_id + "&entity_type=city";
+
 
             // append any cuisine codes
             reqURL = reqURL + "&cuisines=";
@@ -200,14 +235,16 @@ function getLocations(loc) {
                       // console.log(data);
                       // get restaurants array
                       var all_rest = data["restaurants"];
+                      myResults = myResults.concat(data["restaurants"]);
                       // console.log(all_rest);
                       //console.log(all_rest[0]);
                       // var address = all_rest[0]["restaurant"]["location"]["address"];
                       // alert(name + " at " + address);
+                      sortByRating();
 
                       all_rest.forEach(function(rest) {
                       var result = document.createElement("p");
-                      result.innerHTML = rest["restaurant"]["name"] + " at " 
+                      result.innerHTML = rest["restaurant"]["name"] + " at "
                           + rest["restaurant"]["location"]["address"];
                       resultsDiv.append(result);
                       });
@@ -220,21 +257,61 @@ function getLocations(loc) {
 }
 
 function sortByRating() {
+  var resultsDiv = document.getElementById("results");
+  // erase contents of div
+  resultsDiv.innerHTML = "";
+
   myResults.forEach(function(rest) {
     var rating = rest["restaurant"]["user_rating"]["aggregate_rating"];
     // console.log(rating);
-    rating = Math.round(rating);
+    var r = Math.round(rating);
     // console.log("new: " + rating);
-    myRatings.forEach(function(stars) {
-      // console.log(stars);
-      if (rating >= stars && rating < (stars + 0.5)) {
-        console.log(rest["restaurant"]["name"] + " at "
-          + rest["restaurant"]["location"]["address"]);
-      }
-    });
+
+    if (myRatings.length > 0) {
+      myRatings.forEach(function(stars) {
+        // console.log(stars);
+        if (r == stars) {
+          // console.log(rest["restaurant"]["name"] + " at "
+          //   + rest["restaurant"]["location"]["address"]);
+
+          // fill contents with new results
+          var result = document.createElement("p");
+          result.innerHTML = rest["restaurant"]["name"] + " at "
+            + rest["restaurant"]["location"]["address"]
+            + "\n\t Rating: " + rating;
+          resultsDiv.append(result);
+        }
+      });
+    }
+    else {
+      // fill contents with new results
+      var result = document.createElement("p");
+      result.innerHTML = rest["restaurant"]["name"] + " at "
+        + rest["restaurant"]["location"]["address"]
+        + "\n\t Rating: " + rating;
+      resultsDiv.append(result);
+    }
+  });
+}
+
+function displayResults() {
+  var resultsDiv = document.getElementById("results");
+  // erase contents of div
+  resultsDiv.innerHTML = "";
+
+  myResults.forEach(function(rest) {
+    // console.log(r["restaurant"]["name"] + " at "
+    //   + r["restaurant"]["location"]["address"]);
+
+    // fill contents with new results
+    var result = document.createElement("p");
+    result.innerHTML = rest["restaurant"]["name"] + " at "
+      + rest["restaurant"]["location"]["address"];
+    resultsDiv.append(result);
   });
 }
 
 // do this when the document is ready -- happens BEFORE window load
 // $(document).ready(getCheckboxes());
 window.onload = getCheckboxes;
+window.onload = uncheckCheckboxes
